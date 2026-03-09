4ba63f8df93a653254c014595df71d8361d16037
kingfrog711 <gunata.sakri@gmail.com>
2026-03-09 19:49:28 +0700

    [GREEN] Implement Payment model with Voucher Code and Cash On Delivery logic.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,5 +12,33 @@ public class Payment {
     Order order;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+
+        if ("VOUCHER_CODE".equals(method)) {
+            String code = paymentData.get("voucherCode");
+            if (code != null && code.length() == 16 && code.startsWith("ESHOP")) {
+                long numCount = code.chars().filter(Character::isDigit).count();
+                if (numCount == 8) {
+                    this.status = "SUCCESS";
+                } else {
+                    this.status = "REJECTED";
+                }
+            } else {
+                this.status = "REJECTED";
+            }
+        } else if ("CASH_ON_DELIVERY".equals(method)) {
+            String address = paymentData.get("address");
+            String deliveryFee = paymentData.get("deliveryFee");
+            if (address == null || address.trim().isEmpty() || deliveryFee == null || deliveryFee.trim().isEmpty()) {
+                this.status = "REJECTED";
+            } else {
+                this.status = "SUCCESS";
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
